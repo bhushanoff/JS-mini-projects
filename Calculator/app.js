2a54721c045425963ecf95884595b6958090b9a1
bhushanoff <bhushanoffone@gmail.com>
2019-08-10 14:48:23 +0530

    removed bugs from textContent

--- Calculator/app.js
+++ Calculator/app.js
@@ -21,19 +21,34 @@ var calculator = document.querySelector('.calculator')
 var keys = calculator.querySelector('.calculator__keys')
 var display = document.querySelector('.calculator__display')
 var previousKeyType = calculator.dataset.previousKeyType;
-
+var i=0;
 keys.addEventListener('click', e => {
 
   if (e.target.matches('button')) {
-      var key = e.target
-      var action = key.dataset.action
-      var keyContent = key.textContent
-      var displayedNum = display.textContent
-  
+    var key = e.target
+    var action = key.dataset.action
+    var keyContent = key.textContent
+    var displayedNum = display.textContent
+
     if (!action) {
-      if (displayedNum === '0' || previousKeyType === 'operator') {
-        display.textContent = keyContent
-      } else {
+      if(i > 0)
+      {
+        if(i == 1)
+        {
+          display.textContent = keyContent;
+          i = 2;
+        }
+        else
+        {
+          display.textContent = displayedNum + keyContent;
+        }
+      }    
+      else if (displayedNum === '0')
+      {       
+        display.textContent = keyContent;
+      } 
+      else 
+      {       
         display.textContent = displayedNum + keyContent
       }
     }
@@ -49,15 +64,22 @@ keys.addEventListener('click', e => {
       
       calculator.dataset.firstValue = displayedNum;
       calculator.dataset.operator = action;
+
+      i = 1
     }
 
     if (action === 'calculate') {
+      i = 0;
+
       var firstValue = calculator.dataset.firstValue;
       var operator = calculator.dataset.operator;
       var secondValue = display.textContent;
 
       display.textContent = calculate(firstValue, operator, secondValue)
-      // ...
+      
+      delete calculator.dataset.operator;
+      delete calculator.dataset.previousKeyType;
+      delete calculator.dataset.firstValue;
     }
 
     Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
