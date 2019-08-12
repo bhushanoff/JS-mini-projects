64bf1f5fa092eb34c9a9bb13b277ed010e7f74a7
bhushanoff <bhushanoffone@gmail.com>
2019-08-12 14:41:49 +0530

    removed cumbersome i, made calculate fn better

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,58 +1,48 @@
 var calculate = (n1, operator, n2) => {
-  let result = ''
-  
-  if (operator === 'add') {
-    result = parseFloat(n1) + parseFloat(n2)
-  }
-  else if (operator === 'subtract') {
-    result = parseFloat(n1) - parseFloat(n2)
-  } 
-  else if (operator === 'multiply') {
-    result = parseFloat(n1) * parseFloat(n2)
-  } 
-  else if (operator === 'divide') {
-    result = parseFloat(n1) / parseFloat(n2)
-  }
-  
-  return result
+  const num1 = parseFloat(n1) , num2 = parseFloat(n2) ;
+        if (operator === 'add') { return num1 + num2 }
+        else if (operator === 'subtract') { return num1 - num2 } 
+        else if (operator === 'multiply') { return num1 * num2 } 
+        else if (operator === 'divide') { return num1 / num2 }
+        
+        return result
 }
 
 var calculator = document.querySelector('.calculator')
 var keys = calculator.querySelector('.calculator__keys')
 var display = document.querySelector('.calculator__display')
 var previousKeyType = calculator.dataset.previousKeyType;
-var i=0;
+
 keys.addEventListener('click', e => {
 
-  if (e.target.matches('button')) {
+  if (e.target.matches('button')) 
+  {
     var key = e.target
     var action = key.dataset.action
     var keyContent = key.textContent
     var displayedNum = display.textContent
 
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+
     if (!action) {
-      if(i > 0)
-      {
-        if(i == 1)
-        {
-          display.textContent = keyContent;
-          i = 2;
-        }
-        else
-        {
-          display.textContent = displayedNum + keyContent;
-        }
-      }    
-      else if (displayedNum === '0')
-      {       
-        display.textContent = keyContent;
-      } 
-      else 
-      {       
-        display.textContent = displayedNum + keyContent
-      }
+          if(calculator.dataset.previousKeyType) // either no previousType or if p..Type is an operator
+          {
+            if(calculator.dataset.previousKeyType == 'number')
+            {
+              display.textContent += keyContent;
+            }
+            else if(calculator.dataset.previousKeyType != 'number')
+            {
+              display.textContent = keyContent;
+            }
+          }
+          else
+          {
+            display.textContent = keyContent;
+          }
+          calculator.dataset.previousKeyType = 'number'
     }
-    
+
     if (action === 'decimal') {
       display.textContent = displayedNum + '.'
     }
@@ -64,8 +54,6 @@ keys.addEventListener('click', e => {
       
       calculator.dataset.firstValue = displayedNum;
       calculator.dataset.operator = action;
-
-      i = 1
     }
 
     if(action === 'clear'){
@@ -78,13 +66,11 @@ keys.addEventListener('click', e => {
     }
 
     if (action === 'calculate') {
-      i = 0;
-
       var firstValue = calculator.dataset.firstValue;
       var operator = calculator.dataset.operator;
       var secondValue = display.textContent;
 
-      display.textContent = calculate(firstValue, operator, secondValue)
+      display.textContent = calculate(firstValue, operator, secondValue);
       
       delete calculator.dataset.operator;
       delete calculator.dataset.previousKeyType;
