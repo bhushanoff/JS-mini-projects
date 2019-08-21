6b06db66a7d0a60eb3819075a3c11c94cfad719c
bhushanoff <bhushanoffone@gmail.com>
2019-08-21 18:41:02 +0530

    added multiple TDL functionality

---
+++ Better-To-Do-List/app.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+//new folder has direct input for name initially with "correct" button.
+//after putting name, "correct" button goes and replaced by "add", "edit", "trash" button
+// <i class="fas fa-check-circle"></i>
+// <i class="fas fa-plus-circle" id="plus"></i>
+// <i class="fas fa-edit"></i>
+// <i class="fas fa-trash-alt"></i>
+
+// class of parentFolder --> parent-folder
+// class of FolderDiv --> folder-div
+// class of FolderSection --> folder-section
+// class of smallPlusButton --> parent-folder
+
+var parentFolder = document.getElementById('parent-folder');
+var bigPlusButton = document.querySelector('#big-plus');
+var container = document.querySelector('.container');
+var i = 0;
+
+bigPlusButton.addEventListener("click", function(){
+    if(i%3==0){
+        console.log('i%3==0');
+        var lastFolderDiv = document.createElement("div");
+        lastFolderDiv.className = 'folder-div';
+        parentFolder.appendChild(lastFolderDiv);
+    }
+
+    var folderDiv = document.querySelectorAll('.folder-div');
+    console.log(folderDiv[folderDiv.length-1]);
+
+    var lastFolderDiv = parentFolder.lastChild;
+
+    var lastFolderSection = document.createElement("div");
+    lastFolderSection.className = 'folder-section';
+    lastFolderDiv.append(lastFolderSection);
+    console.log(lastFolderSection);
+    
+    var inputFolder = document.createElement("input");
+    var smallPlusButton = document.createElement("i");
+
+    inputFolder.type = "text"; inputFolder.className = 'input-folder';     inputFolder.placeholder = "New Folder";
+    smallPlusButton.className = 'fas fa-plus-circle fa-2x'; smallPlusButton.className = "small-plus";
+
+    lastFolderSection.append(inputFolder);
+    lastFolderSection.append(smallPlusButton);
+    lastFolderDiv.append(lastFolderSection);
+
+    i++;
+    console.log('i has value ',i);
+    
+})
